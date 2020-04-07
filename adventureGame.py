5e7fa528b73790b2304d19037bcb9cc655d37cbf
mohammad <mohammadazawi1@gmail.com>
2020-04-07 16:13:39 -0400

    Add house function

--- adventureGame.py
+++ adventureGame.py
@@ -45,3 +45,46 @@ def cave(item, option):
         print_pause("\nYou walk back out to the field.\n")
         item.append("sward")
     field(item, option)
+
+
+
+def house(item, option):
+    '''Take two prams {item} and {option}
+       display your options and items in the House.
+    '''
+    
+    print_pause("\nYou approach the door of the house.")
+    print_pause("\nYou are about to knock when the door "
+                "opens and out steps a " + option + ".")
+    print_pause("\nEep! This is the " + option + "'s house!")
+    print_pause("\nThe " + option + " attacks you!\n")
+    if "sward" not in item:
+        print_pause("You feel a bit under-prepared for this, "
+                    "what with only having a tiny dagger.\n")
+    while True:
+        choice2 = input("Would you like to (1) fight or (2) "
+                        "run away?")
+        if choice2 == "1":
+            if "sward" in item:
+                print_pause("\nAs the " + option + " moves to attack, "
+                            "you unsheath your new sword.")
+                print_pause("\nThe Sword of Ogoroth shines brightly in "
+                            "your hand as you brace yourself for the "
+                            "attack.")
+                print_pause("\nBut the " + option + "takes one look at "
+                            "your shiny new toy and runs away!")
+                print_pause("\nYou have rid the town of the " + option +
+                            ". You are victorious!\n")
+            else:
+                print_pause("\nYou do your best...")
+                print_pause(f"but your dagger is no match for the {option}.")
+                print_pause("\nYou have been defeated!\n")
+            play_again()
+            break
+        if choice2 == "2":
+            print_pause("\nYou run back into the field. "
+                        "\nLuckily, you don't seem to have been "
+                        "followed.\n")
+            field(item, option)
+            break
+    
