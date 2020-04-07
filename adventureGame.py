23f1f19956614ed42e43983ae95091f6d4e88cff
mohammad <mohammadazawi1@gmail.com>
2020-04-07 16:04:06 -0400

    Add print_pause function

--- adventureGame.py
+++ adventureGame.py
@@ -1,2 +1,9 @@
 import time
 import random
+
+def print_pause(message, sleep_time=2):
+    '''Take a string {message} and an integer {sleep_time}
+       print {message}, and pass{sleep_tome} to sleep function
+    '''
+    print(message)
+    time.sleep(sleep_time)
