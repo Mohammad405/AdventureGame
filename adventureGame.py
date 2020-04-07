b1fe4a92b5e7052d0cb88d35614396817578d856
mohammad <mohammadazawi1@gmail.com>
2020-04-07 16:07:39 -0400

    Add intro function

--- adventureGame.py
+++ adventureGame.py
@@ -7,3 +7,18 @@ def print_pause(message, sleep_time=2):
     '''
     print(message)
     time.sleep(sleep_time)
+
+
+def intro(item, option):
+    ''' Take two prams {item} and {option}
+        and display intro messades
+    '''
+    print_pause("You find yourself standing in an open field, filled "
+                "with grass and yellow wildflowers.\n")
+    print_pause("Rumor has it that a " + option + " is somewhere around "
+                "here, and has been terrifying the nearby village.\n")
+    print_pause("In front of you is a house.\n")
+    print_pause("To your right is a dark cave.\n")
+    print_pause("In your hand you hold your trusty (but not very "
+                "effective) dagger.\n")
+
